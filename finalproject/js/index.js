379f6f3c5f47c7d12cd23d4d7d7e785f7cb91fd0
asbjacoby <asbjacoby@gmail.com>
2015-11-16 19:01:17 -0500

    final project so far
    as of 11/16

--- finalproject/js/index.js
+++ finalproject/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-$('.has-children').click(function(event){
+/***$('.has-children').click(function(event){
 	$(".dropdown", this).slideDown(); 
 	event.preventDefault();
   });
@@ -9,7 +9,8 @@ $('.dropdown, #contact').mouseleave(function(event){
     $('#contact', this).slideUp(); 
     event.preventDefault();
   });
- 
+ ***/
+
 $("#slideshow > div:gt(0)").hide();
 
 setInterval(function() { 
@@ -21,4 +22,16 @@ setInterval(function() {
     .appendTo('#slideshow');
 },  3000);
 
+$("#bookslideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#bookslideshow > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(3000)
+    .end()
+    .appendTo('#bookslideshow');
+},  3000);
+
 });
+
